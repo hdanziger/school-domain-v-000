272bce50315d11507c16409c725422b8d947dbdf
Hannah Danziger <hdanziger8@gmail.com>
2017-09-26 18:33:20 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,6 +3,19 @@ class School
 
   def initialize(name)
     @name = name
+    @roster = {}
   end
+
+  def add_student (name, grade)
+    puts "#{name}, #{grade}"
+
+    roster[grade] = []
+    roster[grade] << name
+
+    roster
+
+  end
+
+
 
 end
